eecf3ec63ab2c9cd6cf67a74461083cd0b1f61cc
Leejin-Yang <ejin1996@gmail.com>
2022-10-06 16:14:32 +0900

    feat: 페이지 렌더링 구현

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,10 @@
+function AboutPage() {
+  return (
+    <main>
+      <h1>about</h1>
+      <button type='button'>go main</button>
+    </main>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/RootPage.tsx
@@ -0,0 +1,10 @@
+function RootPage() {
+  return (
+    <main>
+      <h1>root</h1>
+      <button type='button'>about</button>
+    </main>
+  )
+}
+
+export default RootPage
